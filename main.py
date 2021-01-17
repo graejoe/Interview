bd64d474e1e4c568b3786941763841c48d5a8b47
Joe Graefe <graejoe@msn.com>
2021-01-17 16:04:44 -0700

    updates

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 from fastapi import FastAPI, File, UploadFile
+import urllib.parse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root(q: str = None):
-    if(q == None):
-        question = "Invalid or missing question"
-        sum = "Missing Question"
-    elif(q.find('+') != -1):
+    query = urllib.parse.unquote_plus(q)
+    if(query == 'What is your name?'):
+        return "Joe Graefe"
+    if(query == 'PING'):
+        return "PONG"
+    if(query == "What is your quest?"):
+        return "coding"
+
+    if(q[0].isnumeric()):
         sum = 0
-        question = ""
-        numbers = q.split('+')
+        numbers = q.split(' ')
         for number in numbers:
-            sum += int(number)
-        
-        for item in q:
-            question += item
-    return {"Question": question,
-        "Answer" : sum    }
+            if(number.isnumeric()):
+                sum += int(number)
+        return sum
+    else:
+        listvowels = ['a', 'e', 'i', 'o', 'u']
+        words = q.split(' ')
+        consonants = 0
+        vowels = 0
+        for word in words:
+            for letter in word:
+                if(letter in listvowels):
+                    vowels += 1
+                else:
+                    consonants += 1
+
+        return "{}-{}-{}".format(len(words), consonants, vowels)
+
+    return query
